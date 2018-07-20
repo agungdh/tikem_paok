8cbcb2614eded748364ae8ca4ddeab7c51930d75
agungdh <agungdh@live.com>
2018-07-21 00:25:43 +0700

    update menu admin

--- application/views/template/menu_admin.php
+++ application/views/template/menu_admin.php
@@ -1,5 +1,7 @@
 <li><a class="app-menu__item" href="<?php echo base_url(); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
 
+<li><a class="app-menu__item" href="<?php echo base_url('fakultas'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Fakultas</span></a></li>
+
 <li><a class="app-menu__item" href="<?php echo base_url('kegiatan'); ?>"><i class="app-menu__icon fa fa-book"></i><span class="app-menu__label">Kegiatan</span></a></li>
 
 <li><a class="app-menu__item" href="<?php echo base_url('user'); ?>"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">User</span></a></li>
