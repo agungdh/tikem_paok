5077691ff0555155a937b56b267c7a463934ed58
agungdh <agungdh@live.com>
2018-07-21 02:17:31 +0700

    apus gapenting

--- application/views/config/index.php
+++ application/views/config/index.php
@@ -40,37 +40,6 @@
           </div>
     </div>
   </div>
-
-  <div class="col-md-12">
-    <div class="tile">
-      <div class="tile-body">
-        
-          <h3 class="tile-title">Zenziva Api</h3>
-          
-          <div class="form-group">
-            <label class="control-label">Zenziva Userkey</label>
-            <input class="form-control" type="text" value="<?php echo $data['config']->zenziva_userkey; ?>" required placeholder="Masukan Zenziva Userkey" name="data[zenziva_userkey]">
-          </div>
-
-          <div class="form-group">
-            <label class="control-label">Zenziva Passkey</label>
-            <input class="form-control" type="text" value="<?php echo $data['config']->zenziva_passkey; ?>" required placeholder="Masukan Zenziva Passkey" name="data[zenziva_passkey]">
-          </div>
-
-          <div class="form-group">
-            <label class="control-label">Zenziva SMS [Total: <normalText id="total"></normalText> | Sisa: <normalText id="sisa"></normalText>]</label>
-            <textarea class="form-control" id="sms" name="data[zenziva_sms]" placeholder="Tulis SMS disini" required><?php echo $data['config']->zenziva_sms; ?></textarea>
-          </div>
-
-
-          </div>
-          <div class="tile-footer">
-            <button class="btn btn-primary simpan" type="button"><i class="fa fa-fw fa-lg fa-check-circle"></i>Simpan</button>
-            &nbsp;&nbsp;&nbsp;
-            <a class="btn btn-secondary" href="<?php echo base_url(); ?>"><i class="fa fa-fw fa-lg fa-times-circle"></i>Batal</a> <input type="submit" style="visibility: hidden;">
-          </div>
-    </div>
-  </div>
 </div>
 
 </form>
